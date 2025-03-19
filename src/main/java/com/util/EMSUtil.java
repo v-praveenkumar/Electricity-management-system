77328e99d2bddc5737f8453cd725da844b0c2541
v-praveenkumar <117658022+v-praveenkumar@users.noreply.github.com>
2025-03-19 23:16:21 +0530

    Update EMSUtil.java

--- src/main/java/com/util/EMSUtil.java
+++ src/main/java/com/util/EMSUtil.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 public class EMSUtil {
   
 //      private static final String URL = "jdbc:derby:C:\\Users\\2824553\\MyDB;create=true";
-      private static final String URL = "jdbc:derby:C:\\Users\\SATHISH\\MyDB;create=true";
+      private static final String URL = "jdbc:derby:C:\\Users\\praveen\\MyDB;create=true";
 
       public static Connection createConnection() throws SQLException, ClassNotFoundException {
     	  Class.forName("org.apache.derby.jdbc.EmbeddedDriver"); 
